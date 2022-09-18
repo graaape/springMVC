979280b7237e145af34cd493d05ea897cd8fe7fc
Chen <dedicatus927@gmail.com>
2022-09-18 21:18:51 +0800

    4

---
+++ ssmbuild/src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+//    service调dao层：组合dao
+    private BookMapper bookMapper;
+    public void setBookMapper(BookMapper bookMapper){
+        this.bookMapper=bookMapper;
+    }
+    @Override
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBooks() {
+        return bookMapper.queryAllBooks();
+    }
+}
